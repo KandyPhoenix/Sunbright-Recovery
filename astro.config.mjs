b59e3494eb9df33e81b8fc1de7f551c08bbd7412
Kandy <kandyphoenix@hotmail.com>
2026-05-28 12:42:20 -0500

    Fix domain to .org, remove flyer copy from V2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sunbrightrecovery.com',
+  site: 'https://sunbrightrecovery.org',
   integrations: [
     tailwind({
       applyBaseStyles: false,
